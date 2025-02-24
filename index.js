d79fe7b65f3cbcd3bc47ef7c51fe8cd06aa33ad2
lyrium-haze <lyrium.haze@gmail.com>
2025-02-24 23:49:30 +0300

    finished intro

---
+++ index.js
@@ -0,0 +1,10 @@
+const topBar = document.querySelector(".top-bar");
+
+window.addEventListener("scroll", () => {
+  topBar.classList.toggle("is-pinned", window.scrollY > 0);
+});
+
+document.querySelector('.phone').addEventListener('input', function (e) {
+    let x = e.target.value.replace(/\D/g, '').match(/(\d{1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+    e.target.value = !x[2] ? "+7" : "+7 (" + x[2] + (x[3] ? ") " + x[3] : "") + (x[4] ? "-" + x[4] : "") + (x[5] ? "-" + x[5] : "");
+});
